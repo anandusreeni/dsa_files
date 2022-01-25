7485cfd7c70ec1ee4f9dbdbb08743b885c5562f3
Anandu Sreenivasan <45094650+anandusreeni@users.noreply.github.com>
2022-01-25 14:21:11 +0530

    Update and rename week13_QandA_activity.sql to Group2_week13_QandA_activity.sql

--- Group2_week13_QandA_activity.sql
+++ Group2_week13_QandA_activity.sql
@@ -12,8 +12,8 @@ Date_of_joining DATE NOT NULL);
 INSERT INTO employees(Employee_ID,Name,Gender,Department,Salary,DOB,Date_of_joining) values 
 	  ('CP0123', 'Ann Mery','F','HR',45000,str_to_date('10/OCT/1989', "%d/%b/%Y"),str_to_date('1/Jan/2018', "%d/%b/%Y")),
 	  ('CP0087', 'Felix M','M','Finance',48000,str_to_date('12/Apr/1981', "%d/%b/%Y"),str_to_date('10/Dec/2000', "%d/%b/%Y")),
-      ('CP0197', 'Merlin','M','CEO',80000,str_to_date('01/Mar/1990', "%d/%b/%Y"),str_to_date('10/May/2011', "%d/%b/%Y")),
-      ('CP0213', 'Philip','M','Retail',47000,str_to_date('01/Apr/1991', "%d/%b/%Y"),str_to_date('11/Jun/2012', "%d/%b/%Y")),
+          ('CP0197', 'Merlin','M','CEO',80000,str_to_date('01/Mar/1990', "%d/%b/%Y"),str_to_date('10/May/2011', "%d/%b/%Y")),
+          ('CP0213', 'Philip','M','Retail',47000,str_to_date('01/Apr/1991', "%d/%b/%Y"),str_to_date('11/Jun/2012', "%d/%b/%Y")),
 	  ('CP0243', 'Michael','M','Retail',40000,str_to_date('01/Dec/1992', "%d/%b/%Y"),str_to_date('30/May/2016', "%d/%b/%Y")),
 	  ('CP0289', 'Susan','F','Retail',40000,str_to_date('01/Jan/1991', "%d/%b/%Y"),str_to_date('01/Apr/2016', "%d/%b/%Y")),
 	  ('CP0298', 'Abram','M','Relations',30000,str_to_date('17/Apr/1994', "%d/%b/%Y"),str_to_date('06/OCt/2016', "%d/%b/%Y")),
@@ -38,7 +38,7 @@ SELECT * FROM employees WHERE DEPARTMENT="HR";
 CREATE TABLE female_employees
   AS (SELECT * FROM employees WHERE Gender="F");
 
-select * from female_employees;
+SELECT * FROM female_employees;
 
 
 
