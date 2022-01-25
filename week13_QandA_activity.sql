1cf4a0d0852ac27e0f7ca8ff54e70edd5acd1b1c
Anandu Sreenivasan <45094650+anandusreeni@users.noreply.github.com>
2022-01-25 14:19:00 +0530

    Update and rename week13_qanda_activity.sql to week13_QandA_activity.sql

--- week13_QandA_activity.sql
+++ week13_QandA_activity.sql
@@ -1,3 +1,5 @@
+-- Create the required Table 
+
 CREATE TABLE employees (
 Employee_ID VARCHAR(10) PRIMARY KEY,
 Name VARCHAR(30) NOT NULL,
@@ -6,8 +8,6 @@ Department VARCHAR(30) NOT NULL,
 Salary INT NOT NULL,
 DOB DATE NOT NULL,
 Date_of_joining DATE NOT NULL);
-
-
 
 INSERT INTO employees(Employee_ID,Name,Gender,Department,Salary,DOB,Date_of_joining) values 
 	  ('CP0123', 'Ann Mery','F','HR',45000,str_to_date('10/OCT/1989', "%d/%b/%Y"),str_to_date('1/Jan/2018', "%d/%b/%Y")),
@@ -20,23 +20,39 @@ INSERT INTO employees(Employee_ID,Name,Gender,Department,Salary,DOB,Date_of_join
 	  ('CP0300', 'Alia','F','Relations',30000,str_to_date('17/Oct/1995', "%d/%b/%Y"),str_to_date('18/OCt/2016', "%d/%b/%Y")),
 	  ('CP0321', 'Raichal','F','Marketing',34000,str_to_date('09/Oct/1990', "%d/%b/%Y"),str_to_date('22/OCt/2016', "%d/%b/%Y")),
 	  ('CP0276', 'Thomas','M','Marketing',44000,str_to_date('19/Nov/1983', "%d/%b/%Y"),str_to_date('22/OCt/2018', "%d/%b/%Y"));
+ 
+ 
       
-      
-select * from employees;
+-- Select the employees from following departments:
+		--Marketing
+		--Retail
+		--HR
 
 SELECT * FROM employees WHERE DEPARTMENT="MARKETING";
 SELECT * FROM employees WHERE DEPARTMENT="RETAIL";
 SELECT * FROM employees WHERE DEPARTMENT="HR";
+
+
+--Create new table with female_employees data only 
 
 CREATE TABLE female_employees
   AS (SELECT * FROM employees WHERE Gender="F");
 
 select * from female_employees;
 
+
+
+--Select maximum,minimum,average salary
+
 SELECT MAX(SALARY) AS "Maximum Salary" ,MIN(SALARY) AS "Minimum Salary",AVG(SALARY) AS "Average Salary" FROM employees;
+
+
+
+--SQL query to display the employee details based on the following conditions.
+	--a. Male employees having salary greater than 40000
+	--b. Female employees having salary less than 45000
+	--c. Employee having salary between 30000 and 60000 and working in Marketing or Retail department.
 
 SELECT * FROM employees where Gender="M" and Salary > 40000;
 SELECT * FROM employees where Gender="F" and Salary < 45000;
-
-
 SELECT * FROM employees where (Department="Marketing" or Department="Retail") and (Salary between 30000 and 60000);
